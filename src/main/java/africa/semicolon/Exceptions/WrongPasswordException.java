4e4c1a7990dd6344ee4532df1233c218634211fc
Megapreneur <ademolamegbabi@gmail.com>
2022-07-11 19:48:44 +0100

    A financial App

--- src/main/java/africa/semicolon/Exceptions/WrongPasswordException.java
+++ src/main/java/africa/semicolon/Exceptions/WrongPasswordException.java
@@ -2,5 +2,6 @@ package africa.semicolon.Exceptions;
 
 public class WrongPasswordException extends RuntimeException {
     public WrongPasswordException(String message) {
+        super(message);
     }
 }
