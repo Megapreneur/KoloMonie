0d514f49a7fc0b32326c4574fcefca2618ea20c4
Megapreneur <ademolamegbabi@gmail.com>
2022-07-13 22:53:21 +0100

    A financial App

--- src/main/java/africa/semicolon/controllers/BankUserController.java
+++ src/main/java/africa/semicolon/controllers/BankUserController.java
@@ -4,6 +4,7 @@ import africa.semicolon.dto.Request.*;
 import africa.semicolon.dto.Response.*;
 import africa.semicolon.services.BankUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class BankUserController {
     public WithdrawalResponse withdrawal(@RequestBody WithdrawalRequest withdrawalRequest){
         return bankUserService.withdrawal(withdrawalRequest);
     }
-    @PostMapping("/bankUser/balance")
+    @GetMapping("/bankUser/balance")
     public CheckBalanceResponse checkBalance(@RequestBody CheckBalanceRequest checkBalanceRequest){
         return bankUserService.checkBalance(checkBalanceRequest);
     }
